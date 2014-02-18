999adf7de6b7be60724c44c11f2d061a3cef6ddb
renansep <renan@Renans-MacBook-Pro-2.local>
2014-02-18 11:50:26 -0300

    Push request

---
+++ ShoppingList/ShoppingList/CadastroViewController.h
@@ -0,0 +1,15 @@
+//
+//  CadastroViewController.h
+//  ShoppingList
+//
+//  Created by Renan Cargnin on 2/18/14.
+//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CadastroViewController : UIViewController <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *txtNome;
+
+@end
